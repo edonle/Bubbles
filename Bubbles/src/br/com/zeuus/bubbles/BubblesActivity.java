ef0b668ef7fdea0175c67780ddfe5e1943e695d3
Edson <edonle@gmail.com>
2012-04-16 22:11:19 -0300

    Primeiro commit do Projeto Bubbles

---
+++ Bubbles/src/br/com/zeuus/bubbles/BubblesActivity.java
@@ -0,0 +1,13 @@
+package br.com.zeuus.bubbles;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BubblesActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
